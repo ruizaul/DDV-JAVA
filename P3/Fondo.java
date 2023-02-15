f4f44d556b9b93cfe0accf818fca62e6675c4f34
ruizaul <fimbressaul@hotmail.com>
2023-02-15 12:20:28 -0700

    fondo aun no se mueve

---
+++ P3/Fondo.java
@@ -0,0 +1,24 @@
+import javax.swing.*;
+
+public class Fondo extends JLabel implements Runnable {
+
+    ImageIcon fondo;
+    String url;
+
+    public Fondo(String url) {
+
+        this.url = url;
+
+        fondo = new ImageIcon(this.getClass().getResource(url));
+        setIcon(fondo);
+
+    }
+
+    public void run() {
+
+        
+
+    }
+
+
+}
